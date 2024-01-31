fafdd2b948b9a2e1f50f30674fd37717c7a3b7b6
Wojciech Malarski <w.malarski@gmail.com>
2024-01-31 20:14:02 +0100

    adds accordion

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,6 @@ const { parkwindPlugin } = require('@park-ui/tailwind-plugin')
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,marko}"],
   theme: { extend: {} },
-  plugins: [require("daisyui"), parkwindPlugin],
+  plugins: [parkwindPlugin],
   darkMode: ['class'],
 };
